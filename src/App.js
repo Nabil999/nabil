82156e1cb30673fcda0c74b2e6859b65d6e7f3a6
nabil999 <nabil.farouk@gmail.com>
2020-10-04 00:04:58 +0200

    another update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1>hello world</h1>
       <h6>this is scc app</h6>
-      <p>now trying to push some updates to the page</p>
+      <p>now trying to push some updates to the page, let's see if it's working</p>
     </div>
   );
 }
