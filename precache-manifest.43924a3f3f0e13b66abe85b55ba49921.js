784dd9c58847bc602ee8fae10df52be941bad11c
nabil999 <nabil.farouk@gmail.com>
2020-10-03 23:49:23 +0200

    Updates

---
+++ precache-manifest.43924a3f3f0e13b66abe85b55ba49921.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e436ccf8113e65f3800e1865b773d99",
+    "url": "/nabil/index.html"
+  },
+  {
+    "revision": "98597faafdcda222f433",
+    "url": "/nabil/static/js/2.66ad45a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nabil/static/js/2.66ad45a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50d2c130de8afcd26b2",
+    "url": "/nabil/static/js/main.d9d60e67.chunk.js"
+  },
+  {
+    "revision": "8eca4dc974ce4e89d190",
+    "url": "/nabil/static/js/runtime-main.0ab5cbae.js"
+  }
+]);
