6ebe54156ae1fdaf7ad265fb2a7b5908e82708bf
nabil999 <nabil.farouk@gmail.com>
2020-10-03 23:52:15 +0200

    adding small paragraph

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <h6>this is scc app</h6>
+      <p>now trying to push some updates to the page</p>
+    </div>
+  );
+}
+
+export default App;
